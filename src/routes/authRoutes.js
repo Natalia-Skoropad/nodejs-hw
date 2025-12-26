57ce0863b5dc46f71449a5450ad6b85b652f7fd2
Nata <skoropad_natalia@ukr.net>
2025-12-26 16:00:47 +0200

    Add password reset via email and user avatar upload

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,15 @@ import {
   logoutUser,
   refreshUserSession,
   registerUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 
 //=====================================================================
@@ -23,6 +27,18 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 
 //=====================================================================
 
